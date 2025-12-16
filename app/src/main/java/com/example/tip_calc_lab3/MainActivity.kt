8e76c412b1658cba42eea62972b11babe08b1a95
ghglolkok-cell <ghglolkok@gmail.com>
2025-12-16 08:28:26 +0700

    demoscreen

---
+++ app/src/main/java/com/example/tip_calc_lab3/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.tip_calc_lab3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.tip_calc_lab3.ui.theme.Tip_Calc_Lab3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tip_Calc_Lab3Theme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    DemoScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun DemoTextPreview() {
+    Tip_Calc_Lab3Theme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DemoScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Калькулятор скидок и чаевых",
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
