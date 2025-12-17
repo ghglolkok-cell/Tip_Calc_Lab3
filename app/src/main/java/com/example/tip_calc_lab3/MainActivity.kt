85b82e044c91e86580f276d87056f322d4279330
ghglolkok-cell <ghglolkok@gmail.com>
2025-12-17 14:59:16 +0700

    summorder

--- app/src/main/java/com/example/tip_calc_lab3/MainActivity.kt
+++ app/src/main/java/com/example/tip_calc_lab3/MainActivity.kt
@@ -6,29 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.tip_calc_lab3.ui.theme.Tip_Calc_Lab3Theme
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tip_calc_lab3.ui.theme.Tip_Calc_Lab3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +54,7 @@ fun DemoTextPreview() {
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
     var order by remember { mutableStateOf("") }
+    var dishCount by remember { mutableStateOf("") }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -86,5 +85,25 @@ fun DemoScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier.width(190.dp)
             )
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Количество блюд
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Text(
+                text = "Количество блюд:",
+                fontSize = 22.sp,
+                modifier = Modifier.width(150.dp)
+            )
+            TextField(
+                value = dishCount,
+                onValueChange = { dishCount = it },
+                modifier = Modifier.width(150.dp)
+            )
+        }
     }
 }
+
